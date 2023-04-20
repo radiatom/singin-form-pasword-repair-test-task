4b4a1aa7cafbf0c5a19222f17fad31f7f62e79e9
radiatom <arssenmozol@gmail.com>
2023-04-20 17:25:21 +0300

    add new component

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,62 @@ import "./App.css";
 import logo from "./img/logo.png";
 
 function App() {
-  const [login, setLogin] = useState("");
-  const handleChangeLogin = (event: any) => {
-    setLogin(event.target.value);
-  };
-  const [password, setpassword] = useState("");
-  const handleChangePassword = (event: any) => {
-    setpassword(event.target.value);
+  // const [login, setLogin] = useState("");
+  // const handleChangeLogin = (event: any) => {
+  //   setLogin(event.target.value);
+  // };
+  // const [password, setPassword] = useState("");
+  // const handleChangePassword = (event: any) => {
+  //   setPassword(event.target.value);
+  // };
+  const [number, setNumber] = useState("");
+  const handleChangeNumber = (event: any) => {
+    setNumber(event.target.value);
   };
   return (
     <div className="App">
       <div className="body">
         <div className="container">
-          <img className="logo" src={logo} alt="logo" />
-          <div className="login">Login</div>
-          <input
-            className="inputLogin"
-            value={login}
-            type="text"
-            onChange={handleChangeLogin}
-          />
-          <div className="password">Password</div>
-          <input
-            className="inputPassword"
-            value={password}
-            type="text"
-            onChange={handleChangePassword}
-          />
-          <div className="fp">Forgot password?</div>
-          <div className="button">
-            <div className="singIn">Sing In</div>
+          {/* <div>
+            <img className="logo" src={logo} alt="logo" />
+            <div className="login">Login</div>
+            <input
+              className="inputLogin"
+              value={login}
+              type="text"
+              onChange={handleChangeLogin}
+            />
+            <div className="password">Password</div>
+            <input
+              className="inputPassword"
+              value={password}
+              type="text"
+              onChange={handleChangePassword}
+            />
+            <div className="fp">Forgot password?</div>
+            <div className="button">
+              <div className="singIn">Sing In</div>
+            </div>
+          </div> */}
+
+          {/* <div>
+            <img className="logo" src={logo} alt="logo" />
+            <div className="pr">Password recovery</div>
+            <div className="enter">Enter your phone number</div>
+            <input
+              className="inputPhoneNumber"
+              value={number}
+              type="text"
+              onChange={handleChangeNumber}
+            />
+            <div className="back">back</div>
+            <div className="buttonCall">
+              <div className="singIn">To call</div>
+            </div>
+          </div> */}
+          <div>
+            <img className="logo" src={logo} alt="logo" />
+            <div className="password">Password: 1234</div>
           </div>
         </div>
       </div>
